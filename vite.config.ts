1909f7e59362a24ed00c80341d08ce0b3f58edd5
Shayan Ali <shayan677@gmail.com>
2025-01-13 00:00:04 +0000

    changes to try and make github pages work

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { BASE, HOME } from "./src/utils/routes";
+import { BASE } from "./src/utils/routes";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `/${BASE}/${HOME}`,
+  base: `/`,
 });
